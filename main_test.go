5fc27a2242a365e14a027f2260648e7ff5f0cbc6
SujithN899 <147419250+SujithN899@users.noreply.github.com>
2023-10-11 17:26:24 -0500

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestWriteAlert(t *testing.T) {
-	// Create a request with sample JSON data for the alert
 	reqData := `{
         "alert_id": "b950482e9911ec7e41f7ca5e5d9a424f",
         "service_id": "my_test_service_id",
@@ -25,33 +24,20 @@ func TestWriteAlert(t *testing.T) {
 		t.Fatal(err)
 	}
 	recorder := httptest.NewRecorder()
-
-	// Call the handler function
 	WriteAlert(recorder, req)
-
-	// Check the HTTP response status code
 	if recorder.Code != http.StatusOK {
 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, recorder.Code)
 	}
-
-	// You can add more assertions to check the response body or verify the database state.
 }
 
 func TestReadAlerts(t *testing.T) {
-	// Create a request with URL parameters
 	req, err := http.NewRequest("GET", "/alerts?service_id=my_test_service_id&alert_ts=1695644060&alert_end_ts=1695644160", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	recorder := httptest.NewRecorder()
-
-	// Call the handler function
 	ReadAlerts(recorder, req)
-
-	// Check the HTTP response status code
 	if recorder.Code != http.StatusOK {
 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, recorder.Code)
 	}
-
-	// You can add more assertions to check the response body or verify the database state.
 }
